34dd04dce95d73729f972afdefa3e05ad29b58a6
Kirk Dirk <jackalex@ya.ru>
2022-09-25 23:19:42 +0500

    add code

--- HWtask002.py
+++ HWtask002.py
@@ -2,4 +2,13 @@
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат
 
-numday = int(input('Введите день недели: '))
+x = input('Введите x(0 или 1): ')
+y = input('Введите y(0 или 1): ')
+z = input('Введите z(0 или 1): ')
+
+leftside = not(x or y or z)
+rightside = not(x) and not(y) and (z)
+if leftside == rightside: print('Истинно!')
+else: print('Ложно!')
+
+
